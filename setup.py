f31f962827c527fbb08e1ca117180944eab40240
Madeleine Ernst <madeleineernst@Madeleines-MacBook-Pro.local>
2019-03-20 11:42:05 +0100

    new version to setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="MetaboDistTrees",
-    version="0.0.8",
+    version="0.0.9",
     author="Madeleine Ernst",
     author_email="mernst@ucsd.edu",
     description="A python implementation for building chemically informed distance trees",
